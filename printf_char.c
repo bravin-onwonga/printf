c07a7729745e0da2c239ec304bbaee325dd02c27
bravin-onwonga <onbravin@gmail.com>
2022-11-27 14:11:51 -0800

    Work done

---
+++ printf_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * printf_char - prints char.
+ * @val: arguments.
+ * Return: 1.
+ */
+
+int printf_char(va_list val)
+{
+	char ch;
+
+	ch = va_arg(val, int);
+	_putchar(ch);
+	return (1);
+}
